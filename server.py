832f68e257e47e38db83528b0b556161068e47fa
GTNOISE User <gtnoise@homenetworklab.noise.gatech.edu>
2014-04-24 01:00:10 +0000

    update router address

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ import traceback
 CLIENT_ADDRESS = 'localhost'
 #SERVER_ADDRESS = '130.207.97.240'
 SERVER_ADDRESS = 'localhost'
-ROUTER_ADDRESS = '50.167.212.31'
+ROUTER_ADDRESS = '192.168.1.2'
 port = 12345
 backlog = 5
 size = 1024
